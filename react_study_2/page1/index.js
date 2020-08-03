5cedb93f4a1224d3f3258096e2edf0ffff8c7491
eka0789 <67857639+eka0789@users.noreply.github.com>
2020-08-03 18:13:12 +0700

    Latihan ReactJS
    Latihan ReactJS

---
+++ react_study_2/page1/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// tempelkan code yang disiapkan dikolom instruksi dibawah
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ react_study_2/page3/Language.js
@@ -0,0 +1,19 @@
+// import React dibawah
+
+
+// nyatakan class Language dibawah
+
+// import React dibawah
+import React from 'react';
+
+// nyatakan class Language dibawah
+class Language extends React.Component {
+  render() {
+    return (
+      <div className='language-item'>
+        <div className='language-name'>HTML & CSS</div>
+        <img className='language-image' src='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg' />
+      </div>
+    );
+  }
+}
